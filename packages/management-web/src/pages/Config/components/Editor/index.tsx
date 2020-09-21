5c0686b8d95d3fded3e21821203c221699683cbb
SammyLiang97 <sammyliang97@gmail.com>
2020-09-21 16:26:11 +0800

    fix(management-web): fix editor can't refresh bug
    Change-Id: Ifc6be104aa97fd5b5af2f68b9f3d7073f54b3a3e

---
+++ packages/management-web/src/pages/Config/components/Editor/index.tsx
@@ -0,0 +1,41 @@
+import React, { FC, useEffect, useState } from 'react';
+import * as monaco from 'monaco-editor';
+
+import styles from './style.less';
+
+type Props = {
+  scope: string;
+  value?: string;
+}
+
+const EDITOR_ID = 'editor';
+
+const Editor: FC<Props> = ({ scope, value = '' }) => {
+  const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor | null>(null);
+
+  useEffect(() => {
+    const e = monaco.editor.create(document.getElementById(EDITOR_ID) as HTMLDivElement, {
+      value: `//${scope}` + value,
+      language: 'json',
+      lineNumbers: 'on',
+      scrollBeyondLastLine: false,
+      automaticLayout: true,
+      contextmenu: false,
+      minimap: {
+        enabled: true,
+      },
+    });
+    setEditor(e);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    editor?.setValue(`//${scope}` + value);
+  }, [editor, scope, value])
+
+  return (
+    <div id={EDITOR_ID} className={styles.editor}></div>
+  );
+};
+
+export default Editor;
--- packages/management-web/src/pages/Config/index.tsx
+++ packages/management-web/src/pages/Config/index.tsx
@@ -1,43 +1,23 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 import { useParams } from 'react-router-dom';
-import * as monaco from 'monaco-editor';
 
 import styles from './style.less';
 import SideMenu from './components/SideMenu';
 import { Button } from 'antd';
+import Editor from './components/Editor';
 
-const EDITOR_ID = 'editor';
 
 const Config: FC = () => {
   const params = useParams<{ configItem: string }>();
 
-  const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor | null>(null);
-
   const { configItem } = params;
-
-
-  useEffect(() => {
-    const editor = monaco.editor.create(document.getElementById(EDITOR_ID) as HTMLDivElement, {
-      value: '',
-      language: 'json',
-      lineNumbers: 'on',
-      scrollBeyondLastLine: false,
-      automaticLayout: true,
-      contextmenu: false,
-      minimap: {
-        enabled: false,
-      },
-    });
-
-    setEditor(editor);
-  }, []);
 
   return (
     <SideMenu>
       Config Page: {configItem}
       <br />
       <div className={styles.container}>
-        <div id={EDITOR_ID} className={styles.editor}></div>
+        <Editor scope={configItem} />
         <div>
           <Button type="primary">Submit</Button>
         </div>
